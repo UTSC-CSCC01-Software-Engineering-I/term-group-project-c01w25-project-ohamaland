95f57f1a436036685d79fd0ab4141143bd3b5788
Rhodwell <rjb.malicdem@gmail.com>
2025-03-08 20:12:21 -0500

    Added group name to email notification

--- backend/api/notifications.py
+++ backend/api/notifications.py
@@ -1,19 +1,20 @@
 from django.core.mail import send_mail
 from django.conf import settings
 
-from .models import GroupMembers
+from .models import Group, GroupMembers
 
 
 def notify_group_receipt_added(group):
+    group = Group.objects.get(id=group)
     members = GroupMembers.objects.filter(group=group)
     for member in members:
         user = member.user
         send_mail(
-            "New Receipt Added to Your Group in Catalog",
+            f"New Receipt Added to {group.name} on Catalog",
             f"Dear {user.username},\nWe would like to inform you that a \
-                new receipt has been added to the group you are a part \
-                    of on Catalog. You can view the details of the new \
-                        receipt within your group’s page.",
+                new receipt has been added to {group.name} on Catalog. \
+                    You can view the details of the new receipt within \
+                        your group’s page.",
             settings.EMAIL_HOST_USER,
             [user.email],
             fail_silently=False,
